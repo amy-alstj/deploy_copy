a7a22a748f1abd19571be34e45f18a6644d65e4c
kimmy190 <90531819+kimmy190@users.noreply.github.com>
2024-03-14 12:54:01 +0900

    Update settings.py

--- mhapy/mhapy/settings.py
+++ mhapy/mhapy/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'd2.cx4esae0ebe0.ca-central-1.rds.amazonaws.com', 
-    '127.0.0.1'
+    '127.0.0.1', 
+    'deploy-production-9524.up.railway.app' 
 ]
 
 # Application definition
